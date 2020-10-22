e692e4a10267e99912027c0e53caa3395ccef460
Lukas Horak <lukas.horak@ackee.cz>
2020-10-22 21:06:22 +0200

    ✨ Add update notifier
    Fixes #18

--- packages/lokse/src/base.ts
+++ packages/lokse/src/base.ts
@@ -1,10 +1,21 @@
 import { Command } from "@oclif/command";
+import * as updateNotifier from "update-notifier";
+
 import { get as getConfig, ConfigType } from "./config";
+
+const pkg = require("../package.json");
 
 export default abstract class Base extends Command {
   protected conf: undefined | null | ConfigType;
 
   async init() {
+    const notifier = updateNotifier({
+      pkg,
+      updateCheckInterval: 1000,
+      shouldNotifyInNpmScript: true
+    });
+    notifier.notify();
+
     this.conf = getConfig();
   }
 }
