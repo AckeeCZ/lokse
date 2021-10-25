9c8e6dcb832f3a68dbf204f94674032df951e629
Filip Kubik <filip.kubik@ackee.cz>
2021-10-25 12:08:17 +0200

    🔥 Remove unused
    Realtes #45

--- packages/plugin-non-breaking-spaces/src/utils.ts
+++ packages/plugin-non-breaking-spaces/src/utils.ts
@@ -1,19 +1,11 @@
-import { Patterns, CustomPatterns } from ".";
+import { mapKeys, mapValues } from "lodash";
+
+import { CustomPatterns } from ".";
 
 export const lowerCaseKeys = (patterns: CustomPatterns) =>
-  Object.keys(patterns).reduce((prev, current) => {
-    prev[current.toLowerCase()] = patterns[current];
-    return prev;
-  }, {} as CustomPatterns);
+  mapKeys(patterns, (_, key) => key.toLowerCase());
 
 const convertStringToRegex = (string: string) => new RegExp(string, "gim");
 
-export const regexifyValues = (patterns: CustomPatterns) => {
-  const regexified: Patterns = {};
-
-  Object.keys(patterns).forEach((key) => {
-    regexified[key] = convertStringToRegex(patterns[key]);
-  });
-
-  return regexified;
-};
+export const regexifyValues = (patterns: CustomPatterns) =>
+  mapValues(patterns, (value) => convertStringToRegex(value));
