aed03649a7e30a592bf10d2c9afef86bf9068784
Lukáš Horák <lukas.horak@ackee.cz>
2022-09-22 15:47:49 +0200

    📝 Add plugin for generating doc page sitemap

--- docs/web/astro.config.mjs
+++ docs/web/astro.config.mjs
@@ -2,14 +2,13 @@ import { defineConfig } from "astro/config";
 import preact from "@astrojs/preact";
 import react from "@astrojs/react";
 
+import sitemap from "@astrojs/sitemap";
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [
-    // Enable Preact to support Preact JSX components.
-    preact(),
-    // Enable React for the Algolia search component.
-    react(),
-  ],
+  integrations: [// Enable Preact to support Preact JSX components.
+  preact(), // Enable React for the Algolia search component.
+  react(), sitemap()],
   site: "https://ackeecz.github.io",
-  base: "/lokse",
+  base: "/lokse"
 });
