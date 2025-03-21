74dd888478590ba64ac9892739d77afa9f325a8b
2bad2furious <22004964+2bad2furious@users.noreply.github.com>
2025-03-21 11:15:16 +0100

    🔧 Remove coverage threshholds

--- vite.config.ts
+++ vite.config.ts
@@ -10,12 +10,6 @@ export default defineConfig({
         include: ['packages/**/*.{test,spec}.ts'],
         coverage: {
             enabled: true,
-            thresholds: {
-                branches: 100,
-                functions: 100,
-                lines: 100,
-                statements: 100,
-            },
             reporter: ['lcov', 'text-summary'],
         },
         outputFile: 'coverage/sonar-report.xml',
