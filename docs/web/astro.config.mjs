f1326ae5f9967e97de5c8afe3d252cc7585efe49
Jiří Čermák <jiri.cermak@ackee.cz>
2025-03-21 10:00:06 +0100

    🔧 Update tsconfig in docs pkg

--- docs/web/astro.config.mjs
+++ docs/web/astro.config.mjs
@@ -1,14 +1,17 @@
-import { defineConfig } from "astro/config";
-import preact from "@astrojs/preact";
-import react from "@astrojs/react";
+import { defineConfig } from 'astro/config';
+import preact from '@astrojs/preact';
+import react from '@astrojs/react';
 
-import sitemap from "@astrojs/sitemap";
+import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [// Enable Preact to support Preact JSX components.
-  preact(), // Enable React for the Algolia search component.
-  react(), sitemap()],
-  site: "https://ackeecz.github.io",
-  base: "/lokse"
+    integrations: [
+        // Enable Preact to support Preact JSX components.
+        preact(), // Enable React for the Algolia search component.
+        react(),
+        sitemap(),
+    ],
+    site: 'https://ackeecz.github.io',
+    base: '/lokse',
 });
